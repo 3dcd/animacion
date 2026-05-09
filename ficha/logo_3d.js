280e255c191a479b1deef86682d3bb0fa25cc7cf
3dcd <151972449+3dcd@users.noreply.github.com>
2026-05-09 19:47:51 -0300

    Add files via upload

--- ficha/logo_3d.js
+++ ficha/logo_3d.js
@@ -550,7 +550,7 @@ an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers)
 	function resizeCanvas() {			
 		var w = lib.properties.width, h = lib.properties.height;			
 		var iw = window.innerWidth, ih=window.innerHeight;			
-		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		var pRatio = Math.min(window.devicePixelRatio || 1, 1.5), xRatio=iw/w, yRatio=ih/h, sRatio=1;			
 		if(isResp) {                
 			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
 				sRatio = lastS;                
